595eb4536b042c31203108c5ff34c5eb81668f3c
sksat <sksat.tec@gmail.com>
2018-09-20 19:38:12 +0900

    [ADD] check output directory(use std::filesystem)

--- Makefile
+++ Makefile
@@ -1,19 +1,25 @@
 TARGET	= mps
 OBJS	= main.o
 
+OUT_DIR	= out
+
 CC	= gcc
 CXX	= g++
 
+CFLAGS	=
+CXXFLAGS= -std=c++17
+LDFLAGS	= -lstdc++fs
+
 %.o: %.c
-	$(CC) -c $< -o $@
+	$(CC) $(CFLAGS) -c $< -o $@
 
 %.o: %.cc
-	$(CXX) -c $< -o $@
+	$(CXX) $(CXXFLAGS) -c $< -o $@
 
 default: $(TARGET)
 
 run: $(TARGET)
-	./$< input/dambreak.prof
+	./$< input/dambreak.prof $(OUT_DIR)
 
 $(TARGET): $(OBJS)
-	$(CXX) -o $@ $(OBJS)
+	$(CXX) -o $@ $(OBJS) $(LDFLAGS)
--- main.cc
+++ main.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <filesystem>
 
 using Float = double;
 
@@ -23,18 +24,72 @@ struct particle_t {
 	vec_t pos, vel, acc;
 };
 
-// .profファイルの読み込み
-void load_data(const std::string &fname, std::vector<particle_t> &particle);
-void save_data(const std::string &fname, std::vector<particle_t> &particle);
 
+// 保存先ディレクトリのチェック
+bool check_outdir(const std::filesystem::path &out_dir);
+// バイト数をいいかんじの文字列にする
+const std::string byte2str(const uintmax_t &size);
+// .profファイルの読み書き
+void load_data(const std::string &fname, std::vector<particle_t> &particle);
+void save_data(const std::string &fname, const std::vector<particle_t> &particle);
+
+// arg: init.prof out_dir
 int main(int argc, char **argv){
+	namespace fs = std::filesystem;
 	std::vector<particle_t> particle;
 
-	if(argc != 2) return -1;
+	if(argc != 3) return -1;
+
+	// output directoryの準備
+	fs::path out_dir(argv[2]);
+	if(!check_outdir(out_dir)) return -1;
 
 	load_data(argv[1], particle);
-	save_data("test.prof", particle);
+
 	return 0;
+}
+
+bool check_outdir(const std::filesystem::path &out_dir){
+	using namespace std::filesystem;
+	if(exists(out_dir)){
+		std::cerr << out_dir << " is already exists." << std::endl;
+		if(!is_directory(out_dir)){
+			std::cerr << out_dir
+				<< " is not a directory. please remove or rename." << std::endl;
+			return false;
+		}
+	}else{
+		std::cerr << "directory " << out_dir << " does not exists." << std::endl;
+		std::cout << "creating " << out_dir << "... ";
+		if(!create_directory(out_dir)){
+			std::cerr << "failed." << std::endl;
+			return false;
+		}
+		std::cout << "ok." << std::endl;
+	}
+
+	auto si = space(out_dir);
+	std::cout << "output directory: " << out_dir
+		<< " (capacity: " << byte2str(si.capacity)
+		<< ", free: " << byte2str(si.free)
+		<< ", available: " << byte2str(si.available)
+		<< ")" << std::endl;
+
+	return true;
+}
+
+const std::string byte2str(const uintmax_t &size){
+	const auto KB = 1024;
+	const auto MB = 1024 * KB;
+	const auto GB = 1024 * MB;
+	if(size > GB)
+		return std::to_string(size / GB) + "GB";
+	else if(size > MB)
+		return std::to_string(size / MB) + "MB";
+	else if(size > KB)
+		return std::to_string(size / KB) + "KB";
+	else
+		return std::to_string(size) + "B";
 }
 
 void load_data(const std::string &fname, std::vector<particle_t> &particle){
@@ -44,7 +99,7 @@ void load_data(const std::string &fname, std::vector<particle_t> &particle){
 	ifs >> num;
 	particle.resize(num);
 
-	std::cout << "loading \"" << fname;
+	std::cout << "loading \"" << fname << "\"";
 
 	for(int i=0;i<num;i++){
 		int n, type;
@@ -65,7 +120,7 @@ void load_data(const std::string &fname, std::vector<particle_t> &particle){
 		<< " (liquid: " << liquid << ", wall: " << wall << ")" << std::endl;
 }
 
-void save_data(const std::string &fname, std::vector<particle_t> &particle){
+void save_data(const std::string &fname, const std::vector<particle_t> &particle){
 	std::ofstream ofs(fname);
 	ofs << particle.size() << std::endl;
 	for(int i=0;i<particle.size();i++){

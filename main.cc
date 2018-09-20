9da901ae85ea39f681f6ab4d58fb49de497aae6f
sksat <sksat.tec@gmail.com>
2018-09-20 18:25:35 +0900

    save data

--- main.cc
+++ main.cc
@@ -15,11 +15,17 @@ struct vec_t {
 
 // 粒子
 struct particle_t {
+	enum type_t {
+		liquid,
+		wall,
+	};
+	type_t type;
 	vec_t pos, vel, acc;
 };
 
 // .profファイルの読み込み
 void load_data(const std::string &fname, std::vector<particle_t> &particle);
+void save_data(const std::string &fname, std::vector<particle_t> &particle);
 
 int main(int argc, char **argv){
 	std::vector<particle_t> particle;
@@ -27,23 +33,46 @@ int main(int argc, char **argv){
 	if(argc != 2) return -1;
 
 	load_data(argv[1], particle);
-	std::cout << "particle num: " << particle.size() << std::endl;
+	save_data("test.prof", particle);
 	return 0;
 }
 
 void load_data(const std::string &fname, std::vector<particle_t> &particle){
 	std::ifstream ifs(fname);
 	int num;
+	int liquid = 0, wall =0;
 	ifs >> num;
 	particle.resize(num);
+
+	std::cout << "loading \"" << fname;
 
 	for(int i=0;i<num;i++){
 		int n, type;
 		auto &p = particle[i];
+		int t;
 		Float prs, pav;
-		ifs >> n >> type
+		ifs >> n >> t
 			>> p.pos.x >> p.pos.y >> p.pos.z
 			>> p.vel.x >> p.vel.y >> p.vel.z
 			>> prs >> pav;
+		p.type = static_cast<particle_t::type_t>(t);
+		if(p.type == particle_t::liquid) liquid++;
+		else if(p.type == particle_t::wall) wall++;
+	}
+
+	std::cout << std::endl;
+	std::cout << "particle num: " << particle.size()
+		<< " (liquid: " << liquid << ", wall: " << wall << ")" << std::endl;
+}
+
+void save_data(const std::string &fname, std::vector<particle_t> &particle){
+	std::ofstream ofs(fname);
+	ofs << particle.size() << std::endl;
+	for(int i=0;i<particle.size();i++){
+		auto &p = particle[i];
+		ofs << i << " " << p.type << " "
+			<< p.pos.x << " " << p.pos.y << " " << p.pos.z << " "
+			<< p.vel.x << " " << p.vel.y << " " << p.vel.z << " "
+			<< 0.0 << " " << 0.0 << std::endl;
 	}
 }
